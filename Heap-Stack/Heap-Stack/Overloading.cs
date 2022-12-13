cb22f3ae7128af5e316ebab77b909aa7a73aa45c
Ali200119 <aliit@code.edu.az>
2022-12-13 04:00:00 +0400

    Complete

---
+++ Heap-Stack/Heap-Stack/Overloading.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Heap_Stack
+{
+	public class Overloading
+	{
+        #region Method Overloading
+
+        public static void Test()
+        {
+            Console.WriteLine("Hello World");
+        }
+
+        public static void Test(string n)
+        {
+            Console.WriteLine(n);
+        }
+
+        public static void Test(string n, int m)
+        {
+            Console.WriteLine($"{n} {m}");
+        }
+
+        #endregion
+    }
+}
+
---
+++ Heap-Stack/Heap-Stack/Program.cs
@@ -0,0 +1,60 @@
+﻿#region Method Overloading
+
+//using Heap_Stack;
+
+//Overloading.Test();
+
+//Overloading.Test("Hello P135");
+
+//Overloading.Test("Run", 7);
+
+#endregion
+
+
+
+#region Heap-Stack
+
+//int a = 5;
+//int b = a;
+//b = 7;
+
+//Console.WriteLine(a);
+//Console.WriteLine(b);
+
+//int[] array = { 10, 23, 16, 8 };
+//int[] array2 = array;
+//array[0] = 7;
+
+//Console.WriteLine(array[0]);
+
+#endregion
+
+
+
+#region Task
+
+Console.BackgroundColor = ConsoleColor.White;
+
+Console.ForegroundColor = ConsoleColor.Black;
+
+Console.WriteLine("Hi. Please, enter a word yo want to reverse:");
+
+string show = Reverse(Console.ReadLine());
+
+Console.WriteLine(show);
+
+static string Reverse(string word)
+{
+    char[] array = word.Trim().ToCharArray();
+
+    string result = "";
+
+    for (int i = array.Length - 1; i >= 0; i--)
+    {
+        result += array[i];
+    }
+
+    return result;
+}
+
+#endregion
